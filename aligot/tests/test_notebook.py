ca81909f8e955a320b409fd9ff3147b1da28af08
skitoo <alexis@skitoo.net>
2015-04-26 17:16:54 +0200

    add test

--- aligot/tests/test_notebook.py
+++ aligot/tests/test_notebook.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.core.urlresolvers import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
@@ -16,21 +16,26 @@ class TestNoteBookApi(TestCase):
         self.client = APIClient()
         self.user = User.objects.create(username='user', password='pass')
         self.client.force_authenticate(user=self.user)
-        self.url = reverse('notebook-list')
 
     def test_create_without_params(self):
-        self.assertEquals(status.HTTP_400_BAD_REQUEST, self.client.post(self.url).status_code)
+        self.assertEquals(status.HTTP_400_BAD_REQUEST, self.client.post(reverse('notebook-list')).status_code)
         self.assertEquals(0, NoteBook.objects.count())
 
     def test_create(self):
-        response = self.client.post(self.url, {'title': 'a title', 'created_by': reverse('user-detail', args=[self.user.id])})
+        response = self.client.post(
+            reverse('notebook-list'),
+            {'title': 'a title', 'created_by': reverse('user-detail', args=[self.user.id])}
+        )
         self.assertEquals(status.HTTP_201_CREATED, response.status_code, response.content)
         self.assertEquals(1, NoteBook.objects.count())
 
     def test_update(self):
         notebook = NoteBook.objects.create(title='a title', created_by=self.user)
         self.assertEquals(1, NoteBook.objects.count())
-        response = self.client.put(reverse('notebook-detail', args=[notebook.id]), {'title': 'new title', 'created_by': reverse('user-detail', args=[self.user.id])})
+        response = self.client.put(
+            reverse('notebook-detail', args=[notebook.id]),
+            {'title': 'new title', 'created_by': reverse('user-detail', args=[self.user.id])}
+        )
         self.assertEquals(status.HTTP_200_OK, response.status_code, response.content)
         self.assertEquals(1, NoteBook.objects.count())
         self.assertEquals('new title', NoteBook.objects.all()[0].title)
@@ -50,8 +55,27 @@ class TestNoteBookApi(TestCase):
         self.assertEquals('a title', response.data['title'], response.data)
 
     def test_get_all(self):
-        nb1 = NoteBook.objects.create(title='notebook 1', created_by=self.user)
-        nb2 = NoteBook.objects.create(title='notebook 2', created_by=self.user)
+        NoteBook.objects.create(title='notebook 1', created_by=self.user)
+        NoteBook.objects.create(title='notebook 2', created_by=self.user)
+        response = self.client.get(reverse('notebook-list'))
+        self.assertEquals(status.HTTP_200_OK, response.status_code, response.content)
+        self.assertEquals(2, len(response.data))
+        self.assertEquals('notebook 1', response.data[0]['title'])
+        self.assertEquals('notebook 2', response.data[1]['title'])
+
+
+class TestNoteBookApiWithDifferentUser(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user1 = User.objects.create(username='user1', password='pass')
+        self.user2 = User.objects.create(username='user2', password='pass')
+        self.client.force_authenticate(user=self.user1)
+        self.url = reverse('notebook-list')
+
+    def test_get_all(self):
+        NoteBook.objects.create(title='notebook 1', created_by=self.user1)
+        NoteBook.objects.create(title='notebook 2', created_by=self.user1)
+        NoteBook.objects.create(title='notebook 3', created_by=self.user2)
         response = self.client.get(reverse('notebook-list'))
         self.assertEquals(status.HTTP_200_OK, response.status_code, response.content)
         self.assertEquals(2, len(response.data))
