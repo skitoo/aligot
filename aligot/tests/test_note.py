190dfa3ceda55b25ce473df0d28a4bd4b14341b0
skitoo <alexis@skitoo.net>
2015-05-06 19:49:46 +0200

    add get test for Note

--- aligot/tests/test_note.py
+++ aligot/tests/test_note.py
@@ -22,13 +22,13 @@ class TestNoteApi(TestCase):
     def test_create(self):
         response = self.client.post(
             reverse('note-list'),
-            {'title': 'a title', 'created_by': self.user.id, 'notebook': self.notebook.id}
+            {'title': 'a title for note', 'created_by': self.user.id, 'notebook': self.notebook.id}
         )
         self.assertEquals(status.HTTP_201_CREATED, response.status_code, response.content)
         self.assertEquals(1, NoteBook.objects.count())
 
     def test_update(self):
-        note = Note.objects.create(title='a title', created_by=self.user, notebook=self.notebook)
+        note = Note.objects.create(title='a title for note', created_by=self.user, notebook=self.notebook)
         self.assertEquals(1, Note.objects.count())
         response = self.client.put(
             reverse('note-detail', args=[note.id]),
@@ -39,8 +39,15 @@ class TestNoteApi(TestCase):
         self.assertEquals('new title', Note.objects.all()[0].title)
 
     def test_delete(self):
-        note = Note.objects.create(title='a title', created_by=self.user, notebook=self.notebook)
+        note = Note.objects.create(title='a title for note', created_by=self.user, notebook=self.notebook)
         self.assertEquals(1, Note.objects.count())
         response = self.client.delete(reverse('note-detail', args=[note.id]))
         self.assertEquals(status.HTTP_204_NO_CONTENT, response.status_code, response.content)
         self.assertEquals(0, Note.objects.count())
+
+    def test_get(self):
+        note = Note.objects.create(title='a title for note', created_by=self.user, notebook=self.notebook)
+        self.assertEquals(1, Note.objects.count())
+        response = self.client.get(reverse('note-detail', args=[note.id]))
+        self.assertEquals(status.HTTP_200_OK, response.status_code, response.content)
+        self.assertEquals('a title for note', response.data['title'], response.data)
