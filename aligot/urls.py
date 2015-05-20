ff8c0aea78098c9a3ee8d0071a8926c66413048e
skitoo <alexis@skitoo.net>
2015-05-20 17:53:58 +0200

    sort imports

--- aligot/urls.py
+++ aligot/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls import include, patterns, url
+from django.conf.urls.static import static
+from django.contrib import admin
 
 from .views import api
-
 
 urlpatterns = patterns(
     '',
