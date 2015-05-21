9484b78350a2c65a61522e379fd7f2dff8690be3
skitoo <alexis@skitoo.net>
2015-05-21 13:53:03 +0200

    install semantic-ui

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,22 @@
 var path = require('path');
-var webpack = require('webpack');
 
 module.exports = {
-    devtool: 'eval',
-    entry: [
-        './aligot/static/src/index'
-    ],
+    entry: './aligot/static/src/index',
     output: {
         path: path.join(__dirname, 'aligot/static'),
-        filename: 'aligot.js',
-        publicPath: '/aligot/static/src/'
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
-    ],
-    resolve: {
-        extensions: ['', '.js', '.jsx']
+        filename: 'aligot.js'
     },
     module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            loaders: ['react-hot', 'babel'],
-            include: path.join(__dirname, 'aligot/static/src')
-        }]
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                include: path.join(__dirname, 'aligot/static/src'),
+                loaders: ['react-hot', 'babel']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
     }
 };
