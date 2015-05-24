7da73e57648105cbcd3bf53059c6a7e62a286599
skitoo <alexis@skitoo.net>
2015-05-24 17:53:06 +0200

    rename app to flux

--- aligot/static/src/flux.jsx
+++ aligot/static/src/flux.jsx
@@ -1,14 +1,16 @@
 import Flummox from 'flummox';
 import NotebookActions from './actions/notebookActions';
 import NotebookStore from './stores/notebookStore';
+import AuthActions from './actions/authActions';
 
 
-export default class App extends Flummox {
+export default class Flux extends Flummox {
     constructor() {
         super();
 
         // actions ------------------------------------------------------- //
         this.createActions('notebooks', NotebookActions);
+        this.createActions('auth', AuthActions);
 
         // stores -------------------------------------------------------- //
         this.createStore('notebooks', NotebookStore, this);
--- aligot/static/src/index.jsx
+++ aligot/static/src/index.jsx
@@ -3,15 +3,11 @@ import Router from 'react-router';
 import FluxComponent from 'flummox/component';
 
 import Aligot from './views/aligot';
-import App from './app';
+import Flux from './flux';
 import routes from './routes';
 
 
-// const app = new App();
-//
-// React.render(<Aligot app={app} />, document.getElementById('aligot'));
-
-let app = new App();
+let flux = new Flux();
 
 var router = Router.create({
     routes: routes,
@@ -20,7 +16,7 @@ var router = Router.create({
 
 router.run((Handler, state) => {
     React.render(
-        <FluxComponent flux={app}>
+        <FluxComponent flux={flux}>
             <Handler {...state} />
         </FluxComponent>,
         document.getElementById('aligot')
