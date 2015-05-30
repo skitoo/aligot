48f9fb417fd9fdebdda64ba4e9dda8dbb8a0ae8d
amirault.quentin@gmail.com <amirault.quentin@gmail.com>
2015-05-30 23:25:06 +0200

    Remove unused import

--- aligot/tests/test_user.py
+++ aligot/tests/test_user.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 
 from django.core.urlresolvers import reverse
-from django.db.utils import IntegrityError
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
