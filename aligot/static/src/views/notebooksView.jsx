a6e899861eca17a55a188288caedc652330d7434
skitoo <alexis@skitoo.net>
2015-05-31 07:52:28 +0200

    refacto notebooks view

--- aligot/static/src/views/notebooksView.jsx
+++ aligot/static/src/views/notebooksView.jsx
@@ -6,8 +6,20 @@ import Sidebar from '../components/sidebar';
 class NotebooksList extends React.Component {
     render() {
         return (
-            <div className="ui link list">
-                {this.props.notebooks.map(notebook => <a className="item">{notebook.title}</a>)}
+            <div className="ui left vertical wide sidebar visible">
+                <div className="item">
+                    <h2 className="center">Carnet de notes</h2>
+                </div>
+                <div className="ui divided list">
+                    {this.props.notebooks.map(notebook =>
+                        <a className="item">
+                            <div className="content">
+                                <div className="header">{notebook.title}</div>
+                                <div className="description">{notebook.note_count} notes</div>
+                            </div>
+                        </a>
+                    )}
+                </div>
             </div>
         );
     }
@@ -25,7 +37,6 @@ export default class NotebooksView extends React.Component {
             <div className="ui pushable">
                 <Sidebar flux={this.props.flux} />
                 <div className="pusher">
-                    <h2>Note</h2>
                     <FluxComponent flux={this.props.flux} connectToStores={['notebooks']}>
                         <NotebooksList notebooks={this.props.notebooks} />
                     </FluxComponent>
