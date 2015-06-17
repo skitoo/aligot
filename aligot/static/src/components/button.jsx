f912b60d40c6c3b0476dd01668875a7b77347176
skitoo <alexis@skitoo.net>
2015-06-17 07:55:46 +0200

    update button component

--- aligot/static/src/components/button.jsx
+++ aligot/static/src/components/button.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 
 export default class Button extends React.Component {
     render() {
-        return <button className={"ui button " + this.props.color}>{this.props.label}</button>
+        return <button className={'ui button ' + this.props.color}>{this.props.label}</button>;
     }
 }
